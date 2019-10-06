c1e0deb01b7afef13aadd74aede86fd0c5dee4ce
noorqidam <noorqidam@gmail.com>
2019-10-06 13:47:10 +0700

    2.add_todo

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text, Style } from 'react-native';
+import { View, Text, TextInput, SafeAreaView } from 'react-native';
+import { Button } from 'native-base'
 
 class App extends Component {
   constructor(props) {
@@ -11,19 +12,39 @@ class App extends Component {
         'Study',
         'Sleep',
         'run'
-      ]
+      ],
+      value: ''
     };
+  }
+  handleSubmit() {
+    this.state.Items.push(this.state.value)
+    this.setState({ value: '' })
   }
 
   render() {
     return (
-      <View>
-        {this.state.Items.map((data) =>
-          <Text style={{ borderBottomWidth: 1, padding: 5, }}>{data}</Text>
-        )
-        }
-
-      </View>
+      <SafeAreaView>
+        <View style={{ margin: 10, flexDirection: 'row' }}>
+          <TextInput
+            style={{ flex: 4, marginRight: 5, borderRadius: 5, borderWidth: 1 }}
+            placeholder="New todo"
+            onChangeText={(text) => this.setState({ value: text })}
+            value={this.state.text}
+          />
+          <Button light
+            style={{ flex: 1, borderWidth: 1, borderColor: 'gray', borderRadius: 5, marginRight: 10, height: 50 }}
+            onPress={() => this.handleSubmit()}
+          >
+            <Text style={{ marginHorizontal: 20 }}>Add</Text>
+          </Button>
+        </View>
+        <View>
+          {this.state.Items.map((data) =>
+            <Text style={{ borderBottomWidth: 1, padding: 5, }}>{data}</Text>
+          )
+          }
+        </View>
+      </SafeAreaView>
     );
   }
 }
