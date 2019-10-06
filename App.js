9b76b590cfac15a8ada999a278910cadcdb18d7b
noorqidam <noorqidam@gmail.com>
2019-10-06 17:25:27 +0700

    add 3.delete_todo

--- App.js
+++ App.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput, SafeAreaView } from 'react-native';
 import { Button } from 'native-base'
-
+import Icon from 'react-native-vector-icons/AntDesign'
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       Items: [
-        'work',
-        'Swim',
-        'Study',
-        'Sleep',
-        'run'
+        { id: 1, name: 'work' },
+        { id: 2, name: 'Swim' },
+        { id: 3, name: 'Study' },
+        { id: 4, name: 'Sleep' },
+        { id: 5, name: 'run' }
       ],
       value: ''
     };
   }
   handleSubmit() {
-    this.state.Items.push(this.state.value)
+    let i = this.state.Items.length
+    this.state.Items.push(
+      {
+        id: i + 1, name: this.state.value
+      }
+    )
     this.setState({ value: '' })
+  }
+
+  handleDelete(id) {
+    for (let i = 0; i < this.state.Items.length; i++) {
+      if (this.state.Items[i].id == (id)) {
+        this.setState((state) => {
+          const list = state.Items.splice(i, 1)
+
+          return list
+        })
+      }
+    }
   }
 
   render() {
@@ -29,22 +46,30 @@ class App extends Component {
             style={{ flex: 4, marginRight: 5, borderRadius: 5, borderWidth: 1 }}
             placeholder="New todo"
             onChangeText={(text) => this.setState({ value: text })}
-            value={this.state.text}
+            value={this.state.value}
           />
           <Button light
             style={{ flex: 1, borderWidth: 1, borderColor: 'gray', borderRadius: 5, marginRight: 10, height: 50 }}
             onPress={() => this.handleSubmit()}
           >
             <Text style={{ marginHorizontal: 20 }}>Add</Text>
+
           </Button>
         </View>
         <View>
           {this.state.Items.map((data) =>
-            <Text style={{ borderBottomWidth: 1, padding: 5, }}>{data}</Text>
+            <View style={{ flexDirection: 'row', borderBottomWidth: 1 }}>
+              <Text style={{ padding: 5, flex: 8, paddingVertical: 20 }}>{data.name}</Text>
+              <Button
+                onPress={() => this.handleDelete(data.id)}
+                style={{ paddingHorizontal: 5, backgroundColor: 'none', marginTop: 5, marginRight: 10 }}>
+                <Icon name="delete" size={30} color='red' />
+              </Button>
+            </View>
           )
           }
         </View>
-      </SafeAreaView>
+      </SafeAreaView >
     );
   }
 }
