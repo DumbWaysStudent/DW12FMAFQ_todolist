4859e9a6371fac1af14a29f34981e2e77e59ec30
noorqidam <noorqidam@gmail.com>
2019-10-07 19:26:28 +0700

    add 5.update_todo

--- App.js
+++ App.js
@@ -1,97 +1,113 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, SafeAreaView, CheckBox } from 'react-native';
-import { Button } from 'native-base'
-import Icon from 'react-native-vector-icons/AntDesign'
+import { View, Text, SafeAreaView, TextInput, CheckBox } from 'react-native';
+import { Button } from "native-base";
+import Icon from "react-native-vector-icons/AntDesign";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      Items: [
-        { id: 1, name: 'work', status: false },
-        { id: 2, name: 'Swim', status: false },
-        { id: 3, name: 'Study', status: false },
-        { id: 4, name: 'Sleep', status: false },
-        { id: 5, name: 'run', status: false }
+      todoListItems: [
+        { id: 1, name: 'Work', done: false },
+        { id: 2, name: 'Swim', done: false },
+        { id: 3, name: 'Study', done: false },
+        { id: 4, name: 'Sleep', done: false },
+        { id: 5, name: 'run', done: false }
       ],
-      value: ''
-    };
+      value: '',
+      btnTitle: 'Add',
+      currentEditItem: 0,
+      currentEditItemName: ''
+    }
   }
-  handleSubmit() {
-    let i = this.state.Items.length
-    this.setState((state) => {
-      const list = state.Items.push({
-        id: i + 1, name: this.state.value
+
+  handleSubmit(action) {
+    if (action == 'Add') {
+      let currId = this.state.todoListItems.length,
+        newItems = [{
+          id: currId + 1,
+          name: this.state.value,
+          done: false
+        }]
+
+      this.setState({ todoListItems: [...this.state.todoListItems, ...newItems] });
+      this.setState({ value: '' })
+    } else if (action == 'Edit') {
+      let name = this.state.currentEditItemName
+      let index = this.state.todoListItems.findIndex((x) => x.name == name)
+      console.log(index)
+      this.setState((state) => {
+        return state.todoListItems[index].name = state.value
       })
-      return { list, value: '' }
-    })
-    this.setState({ value: '' })
+      this.setState({ value: '', btnTitle: 'Add' })
+    }
   }
 
   handleDelete(id) {
-    for (let i = 0; i < this.state.Items.length; i++) {
-      if (this.state.Items[i].id == (id)) {
-        this.setState((state) => {
-          const list = state.Items.splice(i, 1)
-          return list
-        })
-      }
-    }
+    this.setState({
+      todoListItems: this.state.todoListItems.filter((items) => {
+        return items.id != id
+      })
+    })
   }
 
   handleCheckbox(id) {
-    if (this.state.Items[id - 1].status == false) {
+    let index = this.state.todoListItems.findIndex((x) => x.id == id);
+    if (this.state.todoListItems[index].done == false) {
       this.setState((state) => {
-        const list = state.Items[id - 1].status = true
-        return list
+        return state.todoListItems[index].done = true
       })
     } else {
       this.setState((state) => {
-        const list = state.Items[id - 1].status = false
-        return list
+        return state.todoListItems[index].done = false
       })
     }
+  }
+
+  handleEdit(id, name) {
+    this.setState({ value: name, btnTitle: 'Edit', currentEditItemName: name })
+    this.setState({ currentEditItem: (id - 1) })
   }
 
   render() {
     return (
       <SafeAreaView>
-        <View style={{ margin: 10, flexDirection: 'row' }}>
+        <View style={{ flexDirection: 'row', margin: 5 }}>
           <TextInput
-            style={{ flex: 4, marginRight: 5, borderRadius: 5, borderWidth: 1 }}
-            placeholder="New todo"
+            style={{ flex: 1, borderWidth: 1, marginRight: 5, borderColor: 'gray', borderRadius: 3 }}
             onChangeText={(text) => this.setState({ value: text })}
             value={this.state.value}
           />
           <Button light
-            style={{ flex: 1, borderWidth: 1, borderColor: 'gray', borderRadius: 5, marginRight: 10, height: 50 }}
-            onPress={() => this.handleSubmit()}
-          >
-            <Text style={{ marginHorizontal: 20 }}>Add</Text>
+            style={{ borderRadius: 5, fontWeight: 'bold', padding: 10, height: 50, borderWidth: 1, borderColor: 'gray' }}
+            onPress={() => this.handleSubmit(this.state.btnTitle)}>
+            <Text>{this.state.btnTitle}</Text>
           </Button>
         </View>
         <View>
-          {this.state.Items.map((data) =>
-            <View style={{ flexDirection: 'row', borderBottomWidth: 1 }}>
+          {this.state.todoListItems.map((data) =>
+            <View style={{ width: '100%', borderBottomWidth: 1, padding: 5, flexDirection: 'row' }}>
               <CheckBox
-                value={data.status}
+                value={data.done}
                 onValueChange={() => this.handleCheckbox(data.id)}
-                style={{ marginTop: 15 }}
               />
-              <Text style={{ padding: 5, flex: 8, paddingVertical: 20 }}>{data.name}</Text>
+              <Text style={{ flex: 8, paddingVertical: 10 }}>{data.name}</Text>
               <Button
-                onPress={() => this.handleDelete(data.id)}
-                style={{ paddingHorizontal: 5, backgroundColor: 'none', marginTop: 5, marginRight: 10 }}>
-                <Icon name="delete" size={30} color='red' />
+                style={{ paddingHorizontal: 10, marginRight: 5, backgroundColor: "none", borderWidth: 1, borderColor: 'grey' }}
+                onPress={() => this.handleEdit(data.id, data.name)}>
+                <Icon name="edit" size={20} color="green" />
+              </Button>
+              <Button
+                style={{ paddingHorizontal: 10, backgroundColor: "none", borderWidth: 1, borderColor: 'grey' }}
+                onPress={() => this.handleDelete(data.id)} >
+                <Icon name="delete" size={20} color="red" />
               </Button>
             </View>
-          )
-          }
+          )}
         </View>
-      </SafeAreaView >
+      </SafeAreaView>
     );
   }
 }
-
 
 export default App;
